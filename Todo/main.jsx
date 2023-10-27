6631839cc041a0f8efa46f6aabe0b5725e269707
Gigizixx <giovanacaroline455@gmail.com>
2023-10-27 17:20:57 -0400

    PTAC2

--- Todo/main.jsx
+++ Todo/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './Home/Home.jsx';
 import ToDo from './Todo/todo.jsx';
+import Detalhe from './Detalhe/detalhe';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='Todo' element={<Todo />}></Route>
+        <Route path='/detalhe/:id' element={<Detalhe />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
